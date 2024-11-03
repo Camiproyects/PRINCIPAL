3d7ea7f94a138ced8bd69a877854b784c2263ac7
CamiProyects <andresillopillo2004@gmail.com>
2024-11-02 23:31:10 -0500

    02/11/2024-11:31

--- js/index.js
+++ js/index.js
@@ -1,42 +1,9 @@
-const isLoggedIn = false; // Puedes cambiar esto según tu lógica de sesión
-
-// Función para cargar productos
-async function cargarProductos() {
-    const response = await fetch ('php/Const.Index.php');
-    const data = await response.json();
-
-    const productosDiv = document.getElementById('productos');
-    if (data.error) {
-        productosDiv.innerHTML = data.error;
-        return;
-    }
-
-    if (data.productos.length > 0) {
-        data.productos.forEach(producto => {
-            const productoDiv = document.createElement('div');
-            productoDiv.classList.add('producto');
-
-            // Creamos la estructura del producto incluyendo la imagen
-            productoDiv.innerHTML = `
-                <h3>${producto.nombre}</h3>
-                <img src="uploads/${producto.imagen}" alt="${producto.nombre}" style="width:150px; height:150px;">
-                <p>Precio: $${producto.precio}</p>
-                <button class="button" onclick="agregarAlCarrito('${producto.nombre}', 1, ${producto.precio})">Agregar al Carrito</button>
-            `;
-            productosDiv.appendChild(productoDiv);
-        });
-    } else {25
-        productosDiv.innerHTML = "No hay productos disponibles.";
-    }
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
 
-function agregarAlCarrito(producto, cantidad, precio) {
-    if (!isLoggedIn) {
-        window.location.href = 'Views.Login.html';
-        return;
-    }
-    console.log(`Agregando ${producto} al carrito. Precio: ${precio}`);
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
-
-// Cargar productos al inicio
-cargarProductos();
