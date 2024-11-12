539d16d6619428a9a4ba0273914f6a9420b338d7
Camilo <andresillopillo2004@gmail.com>
2024-11-11 21:09:28 -0500

    Auto commit de cambios diarios - 2024-11-11 21:09

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 function openNav() {
   document.getElementById("sidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("main").style.color= "rgb(13, 59, 111)";
 }
 
 function closeNav() {
@@ -22,19 +23,20 @@ function cargarProductos() {
     .then(data => mostrarProductos(data.productos))
     .catch(error => console.error('Error al cargar productos:', error));
 }
+let totalCompra = 0;
 
 function mostrarProductos(productos) {
   const productosDiv = document.getElementById('productos');
   productosDiv.innerHTML = '';
-
   productos.forEach(producto => {
+
     const productoDiv = document.createElement('div');
     productoDiv.classList.add('producto');
     productoDiv.innerHTML = `
       <h2>${producto.nombre}</h2>
       <img src="uploads/${producto.imagen}" alt="${producto.nombre}">
       <p>Precio: $${producto.precio}</p>
-      <button onclick="agregarAlCarrito('${producto.nombre}', ${producto.precio})">Agregar al carrito</button>
+      <button id="Compra" onclick="agregarAlCarrito('${producto.nombre}', ${producto.precio})">Agregar al carrito</button>
     `;
     
     productosDiv.appendChild(productoDiv);
@@ -43,7 +45,14 @@ function mostrarProductos(productos) {
 
 function agregarAlCarrito(nombre, precio) {
   const comprasList = document.getElementById('comprasList');
+
   const item = document.createElement('li');
   item.textContent = `${nombre} - $${precio}`;
   comprasList.appendChild(item);
+
+  totalCompra += precio;
+
+  const totalDiv = document.getElementById('totalCompra');
+  totalDiv.textContent = `Total: $${totalCompra.toFixed(2)}`;
+
 }
